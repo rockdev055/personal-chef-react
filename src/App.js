e68e1f039d9d4a468bb41300915cb34748f90e41
rockdev55 <steve264225@gmail.com>
2017-08-02 21:46:46 -0700

    Remove unused code from App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 import Nav from './components/Nav'
 import Home from './components/Home'
 import HouseholdsContainer from './components/HouseholdsContainer'
-import HouseholdService from './services/HouseholdService'
 import {
   BrowserRouter as Router,
   Route,
-  Link,
 } from 'react-router-dom'
 
 class App extends Component {
