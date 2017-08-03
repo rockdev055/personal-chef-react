66e9013de4eaabe07fdce6203e9cde6fe5cf6c67
rockdev55 <steve264225@gmail.com>
2017-08-02 21:43:07 -0700

    Update App.js to render HouseholdsContainer component on /households route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import Home from './components/Home'
-import Households from './components/Households'
+import HouseholdsContainer from './components/HouseholdsContainer'
 import HouseholdService from './services/HouseholdService'
 import {
   BrowserRouter as Router,
@@ -10,25 +10,8 @@ import {
   Link,
 } from 'react-router-dom'
 
-const households = [
-  { id: 1, name: "Bosco", address: "7668 Jerde Cliff", monthly_rate: 5761},
-  { id: 2, name: "Lockman", address: "78743 Muller Roads", monthly_rate: 7260},
-  { id: 3, name: "Volkman", address: "9134 Dante Valley", monthly_rate: 1295},
-
-]
-
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      households: []
-    }
-  }
-  componentDidMount() {
-    HouseholdService.fetchHouseholds().then(households => this.setState({
-      households: households
-    }))
-  }
+
   render() {
     return (
       <div className="App">
@@ -36,7 +19,7 @@ class App extends Component {
           <div>
             <Nav />
             <Route exact path="/" component={Home} />
-            <Route path="/households" render={() => <Households households={this.state.households} /> } />
+            <Route path="/households" component={HouseholdsContainer} />
           </div>
         </Router>
       </div>
