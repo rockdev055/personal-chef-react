2a45eb64e06cccc62c548e9ca9f33e5599bffaf2
rockdev55 <steve264225@gmail.com>
2017-08-04 21:31:32 -0700

    Build signup action

--- src/redux/modules/Auth/actions.js
+++ src/redux/modules/Auth/actions.js
@@ -1,8 +1,20 @@
-export const signup = (data) => {
-  console.log(data)
+import ApiService from '../../../services/ApiService'
+import { reset } from 'redux-form'
+
+export const authenticating = () => ({type: 'AUTHENTICATING'})
+
+export const setUser = (user) => ({type: 'AUTH_COMPLETE', user})
+
+export const signup = (data, history) => {
   return dispatch => {
-    dispatch({
-      type: 'SIGN_UP'
-    })
+    dispatch(authenticating())
+    ApiService.signup(data)
+      .then(currentUser => {
+        const { user, token } = currentUser
+        localStorage.setItem('token', token)
+        dispatch(setUser(user))
+        dispatch(reset('signup'))
+        history.replace('/households')
+      })
   }
 }
