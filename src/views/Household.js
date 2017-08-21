f629a8726961f2e3643f108baeaf24cec2628e04
rockdev55 <steve264225@gmail.com>
2017-08-20 23:35:28 -0700

    Start code to add a meal to a household

--- src/views/Household.js
+++ src/views/Household.js
@@ -3,6 +3,12 @@ import { connect } from 'react-redux'
 import numeral from 'numeral'
 
 class Household extends Component {
+
+  handleOnChange = (id) => {
+    const householdId = parseInt(this.props.match.params.id)
+    // make a call to a prop method to add the meal selected to the current household
+  }
+
   render() {
     const { households, meals } = this.props
     const id = parseInt(this.props.match.params.id)
@@ -14,7 +20,7 @@ class Household extends Component {
           <h3>{household.name}</h3>
           <p>{household.address}</p>
           <p>{numeral(household.monthly_rate).format('$0,0.00')}</p>
-          {meals.map(m => (<span key={m.id}><span>{m.name}</span><input type="checkbox" value="`${m.id}`" /></span>))}
+          {meals.map(m => (<span key={m.id}><span>{m.name}</span><input onChange={() => this.handleOnChange(m.id)} type="checkbox" value="`${m.id}`" /></span>))}
         </div>
       )
     } else {
