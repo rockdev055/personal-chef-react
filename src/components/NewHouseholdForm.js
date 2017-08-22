b0dbb216e45414333e6ea415fd58699df496e56c
rockdev55 <steve264225@gmail.com>
2017-08-22 16:02:52 -0700

    Style NewHouseholdForm with flexbox

--- src/components/NewHouseholdForm.js
+++ src/components/NewHouseholdForm.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { css } from 'glamor'
 
 class NewHouseholdForm extends Component {
   render() {
     const { handleSubmit } = this.props
     return (
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Family Name</label>
-        <Field name="name" component="input" type="text" />
-        <label htmlFor="address">Address</label>
-        <Field name="address" component="input" type="text" />
-        <label htmlFor="monthly_rate">Monthly Rate</label>
-        <Field name="monthly_rate" component="input" type="text" />
-        <button type="submit">Create Hosuehold</button>
+        <ul {...ul}>
+          <li {...styles}>
+            <label {...styles2} htmlFor="name">Family Name</label>
+            <Field {...styles3} name="name" component="input" type="text" />
+          </li>
+          <li {...styles}>
+            <label {...styles2} htmlFor="address">Address</label>
+            <Field {...styles3} name="address" component="input" type="text" />
+          </li>
+          <li {...styles}>
+            <label {...styles2} htmlFor="monthly_rate">Monthly Rate</label>
+            <Field {...styles3} name="monthly_rate" component="input" type="text" />
+          </li>
+          <li {...styles}>
+            <button {...btnStyle} type="submit">Create Hosuehold</button>
+          </li>
+        </ul>
       </form>
     )
   }
@@ -21,3 +32,43 @@ class NewHouseholdForm extends Component {
 export default reduxForm({
   form: 'newHousehold'
 })(NewHouseholdForm)
+
+const styles = css({
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  // maxWidth: '800px',
+  marginBottom: '20px'
+})
+
+const ul = css({
+  maxWidth: '800px',
+  margin: '0 auto'
+})
+
+const styles2 = css({
+  flex: '1 0 120px',
+  maxWidth: '220px'
+})
+
+const styles3 = css({
+  flex: '1 0 220px',
+  padding: '15px',  
+  borderRadius: '15px',
+  border: '2px solid gray',
+  ':focus': {
+    outline: 'none'
+  }
+})
+
+const btnStyle = css({
+  marginLeft: 'auto',
+  padding: '8px 16px',
+  border: 'none',
+  background: '#333',
+  color: '#f2f2f2',
+  textTransform: 'uppercase',
+  letterSpacing: '.09em',
+  borderRadius: '2px'
+})
