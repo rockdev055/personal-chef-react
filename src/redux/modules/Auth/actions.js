90674ddfbb3d51f3fac58beba5eedeb56f9c14d5
rockdev55 <steve264225@gmail.com>
2017-09-28 09:42:57 -0700

    Add apiCall action

--- src/redux/modules/Auth/actions.js
+++ src/redux/modules/Auth/actions.js
@@ -1,5 +1,7 @@
 import ApiService from '../../../services/Api'
 import { reset } from 'redux-form'
+import { fetchHouseholdsComplete } from '../Households/actions'
+import { fetchMealsComplete } from '../Meals/actions'
 
 export const authenticating = () => ({type: 'AUTHENTICATING'})
 
@@ -10,6 +12,25 @@ export const authenticationFailure = (errors) => ({type: 'AUTH_FAILURE', errors}
 export const logout = (router) => {
   localStorage.removeItem('token')
   return { type: 'LOGOUT' }
+}
+
+export const apiCall = () => {
+  return dispatch => {
+    dispatch({
+      type: 'APP_LOADING'
+    })
+
+    const householdProm = ApiService.get(`/households`)
+
+    const mealProm = ApiService.get(`/meals`)
+
+    Promise.all([householdProm, mealProm])  
+      .then(values => {
+        dispatch(fetchHouseholdsComplete(values[0]))
+        dispatch(fetchMealsComplete(values[1]))
+        dispatch({type: 'APP_LOADING_COMPLETE'})
+        console.log(values)})  
+  }
 }
 
 export const authenticate = () => {
