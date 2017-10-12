9e48fec03ecbdbfe704a50a6222c5f3257497bf3
rockdev55 <steve264225@gmail.com>
2017-10-11 22:48:39 -0700

    Style buttons in HouseholdsContainer for Clients and Leads

--- src/containers/HouseholdsContainer.js
+++ src/containers/HouseholdsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Household from '../views/Household'
+import LeadDetail from '../views/LeadDetail'
 import ClientsContainer from './ClientsContainer'
 import LeadsContainer from './LeadsContainer'
 import { Route, Switch, Link } from 'react-router-dom'
 import { fetchHouseholds } from '../redux/modules/Households/actions'
+import { Button, Icon } from 'semantic-ui-react'
 
 class HouseholdsContainer extends Component {
   constructor(props) {
@@ -16,11 +18,27 @@ class HouseholdsContainer extends Component {
     return (
       <div className="households-container">
         <div className="household-content">
-          <Link to="/households/clients">Clients</Link>
-          <Link to="/households/leads">Leads</Link>
+          <Link to="/households/clients">
+            <Button animated>
+              <Button.Content visible>Clients</Button.Content>
+              <Button.Content hidden>
+                <Icon name='money' />
+              </Button.Content>
+            </Button>
+          </Link>
+          <Link to="/households/leads">
+            <Button animated>
+              <Button.Content visible>Leads</Button.Content>
+              <Button.Content hidden>
+                <Icon name='left arrow' />
+              </Button.Content>
+            </Button>
+          </Link>
           <Switch>
             <Route exact path="/households/clients" component={ClientsContainer} />
-            <Route path="/households/leads" component={LeadsContainer} />
+            <Route exact path="/households/leads" component={LeadsContainer} />
+            <Route path={`/households/clients/:id`} component={Household} />
+            <Route path={`/households/leads/:id`} component={LeadDetail} />
           </Switch>
         </div>
       </div>
