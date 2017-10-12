9f771b7062070fe88a047d93d602e3e137923460
rockdev55 <steve264225@gmail.com>
2017-10-11 22:57:48 -0700

    Style Clients view to cards

--- src/containers/ClientsContainer.js
+++ src/containers/ClientsContainer.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ClientCard from '../views/ClientCard'
+import { Container } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+const StyledLeads = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
 
 class ClientsContainer extends Component {
   render() {
-    const clients = this.props.clients.map(c => <ClientCard {...c} />)
+    const clients = this.props.clients.map(c => <ClientCard key={c.id} {...c} />)
     return (
-      <div>
-        {clients}
-      </div >
+      <Container>
+        <StyledLeads>
+          {clients}
+        </StyledLeads>
+      </Container>
     )
   }
 }
--- src/views/ClientCard.js
+++ src/views/ClientCard.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import numeral from 'numeral'
+
+const StyledLead = styled.div`
+  width: 31%;
+  text-align: center;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 10px;
+`
+
 
 const ClientCard = (props) => {
   return (
-    <div>
+    <StyledLead>
       <Link to={`/households/clients/${props.id}`}>{props.name}</Link>
-    </div >
+      <h3>{props.address}</h3>
+      <h3>{numeral(props.monthly_rate).format('$0,0.00')}</h3>
+    </StyledLead>
   )
 }
 
