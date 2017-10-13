1a16784d5e9054c77c42c08d8d4cc6c13982293d
rockdev55 <steve264225@gmail.com>
2017-10-12 22:51:26 -0700

    Style MealsContainer

--- src/containers/MealsContainer.js
+++ src/containers/MealsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchMeals } from '../redux/modules/Meals/actions'
 import Meal from '../views/Meal'
+import { Container } from 'semantic-ui-react'
 
 class MealsContainer extends Component {
 
@@ -10,10 +11,10 @@ class MealsContainer extends Component {
   }
   render() {
     return (
-      <div>
+      <Container textAlign='center'>
         <h1>All Meals</h1>
         {this.props.meals.map(m => <Meal key={m.id} {...m} />)}
-      </div>
+      </Container>
     )
   }
 }
