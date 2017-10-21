0e913572a96eca2c08b3ced30e913cc23e1d372f
rockdev55 <steve264225@gmail.com>
2017-10-20 23:06:25 -0700

    Update view for LeadCard

--- src/views/LeadCard.js
+++ src/views/LeadCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import numeral from 'numeral'
 
 const StyledLead = styled.div`
   width: 31%;
@@ -19,6 +20,7 @@ const LeadCard = (props) => {
     <StyledLead>
       <h1><Link to={`/households/leads/${props.id}`}>{props.name}</Link></h1>
       <h3>{props.address}</h3>
+      <h4>Potential Monthly: {numeral(props.monthly_rate).format('$0,0.00')}</h4>
       <button onClick={_onClick}>Convert Client</button>
     </StyledLead>
   )
