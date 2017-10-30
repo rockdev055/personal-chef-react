d51caf55dc982d4694bf1c2b1ec15e4693fc0c48
rockdev55 <steve264225@gmail.com>
2017-10-29 23:19:25 -0700

    Add key to household notes to avoid warning

--- src/components/HouseholdNotes.js
+++ src/components/HouseholdNotes.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { List } from 'semantic-ui-react'
-import format from 'date-fns/format'
+import React from "react";
+import { List } from "semantic-ui-react";
+import format from "date-fns/format";
 
-const HouseholdNotes = ({notes}) => {
-    return (
-        <List>
-            {notes.map(n => <List.Item>{format(new Date(n.created_at), 'MMMM Do, YYYY')} - {n.content}</List.Item>)}
-        </List>
-    )
-}
+const HouseholdNotes = ({ notes }) => {
+  return (
+    <List>
+      {notes.map(n =>
+        <List.Item key={n.id}>
+          {format(new Date(n.created_at), "MMMM Do, YYYY")} - {n.content}
+        </List.Item>
+      )}
+    </List>
+  );
+};
 
-export default HouseholdNotes
+export default HouseholdNotes;
