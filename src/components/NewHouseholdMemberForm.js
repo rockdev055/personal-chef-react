31b4144044eb1dded001fc92db745949e4611838
rockdev55 <steve264225@gmail.com>
2017-11-05 22:40:00 -0800

    Remove unused styling from NewHouseholdMemberForm

--- src/components/NewHouseholdMemberForm.js
+++ src/components/NewHouseholdMemberForm.js
@@ -24,9 +24,7 @@ class NewHouseholdMemberForm extends Component {
           <label htmlFor="phone_number">Phone Number</label>
           <Field name="phone_number" component="input" type="text" />
         </Form.Field>
-        <Form.Button primary>
-          Create Member
-        </Form.Button>
+        <Form.Button primary>Create Member</Form.Button>
       </Form>
     )
   }
@@ -35,43 +33,3 @@ class NewHouseholdMemberForm extends Component {
 export default reduxForm({
   form: 'newHouseholdMember'
 })(NewHouseholdMemberForm)
-
-const styles = css({
-  display: 'flex',
-  flexWrap: 'wrap',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  // maxWidth: '800px',
-  marginBottom: '20px'
-})
-
-const ul = css({
-  maxWidth: '800px',
-  margin: '0 auto'
-})
-
-const styles2 = css({
-  flex: '1 0 120px',
-  maxWidth: '220px'
-})
-
-const styles3 = css({
-  flex: '1 0 220px',
-  padding: '15px',
-  borderRadius: '15px',
-  border: '2px solid gray',
-  ':focus': {
-    outline: 'none'
-  }
-})
-
-const btnStyle = css({
-  marginLeft: 'auto',
-  padding: '8px 16px',
-  border: 'none',
-  background: '#333',
-  color: '#f2f2f2',
-  textTransform: 'uppercase',
-  letterSpacing: '.09em',
-  borderRadius: '2px'
-})
