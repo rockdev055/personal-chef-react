9b1a8e35e20a5a1a0f8f953ee2168bcf0a3acfb8
rockdev55 <steve264225@gmail.com>
2017-11-17 22:09:51 -0800

    Add url to meal index

--- src/views/Meal.js
+++ src/views/Meal.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-export default (props) => (
+export default props =>
   <div>
-    {props.name}
+    <a target="_blank" href={props.url}>
+      {props.name}
+    </a>
   </div>
-)
