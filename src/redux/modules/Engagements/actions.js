7f70f38b71bf3d7cca59cba8438fe7e1f9c9d28d
rockdev55 <steve264225@gmail.com>
2017-12-03 19:22:53 -0800

    Build actions for adding meal to engagement

--- src/redux/modules/Engagements/actions.js
+++ src/redux/modules/Engagements/actions.js
@@ -7,6 +7,26 @@ const addEngagementToHousehold = engagement => {
   }
 }
 
+const addMealToEngagement = (engagementId, mealId, householdId) => {
+  return {
+    type: "ADD_MEAL_TO_ENGAGEMENT",
+    mealId,
+    engagementId,
+    householdId
+  }
+}
+
+export const createEngagementMeal = (engagementId, mealId, householdId) => {
+  return dispatch => {
+    return ApiService.post(
+      `/engagements/${engagementId}/meals/${mealId}/engagement_meals`
+    ).then(mealId =>
+      dispatch(addMealToEngagement(engagementId, mealId, householdId))
+    )
+    dispatch({})
+  }
+}
+
 export const createEngagement = (householdId, date) => {
   return dispatch => {
     return ApiService.post(`/households/${householdId}/engagements`, {
