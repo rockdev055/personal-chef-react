cb751da7bcd646708c924966c5e75e0b23e25569
rockdev55 <steve264225@gmail.com>
2018-03-01 22:57:40 -0800

    Update navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import {
-  Link
-} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 import { css } from 'glamor'
 
 class Nav extends Component {
@@ -12,73 +10,46 @@ class Nav extends Component {
   render() {
     return (
       <Menu secondary {...rules}>
-        <Menu.Item
-          name='home'
-          as={Link}
-          to="/"
-        >
+        <Menu.Item name="home" as={Link} to="/">
           Home
         </Menu.Item>
 
-        {
-          this.props.isAuthenticated ?
+        {this.props.isAuthenticated
+          ? <Menu.Menu position="right">
+              <Dropdown item text="Families">
+                <Dropdown.Menu>
+                  <Link to="/households/new">
+                    <Dropdown.Item>New Family</Dropdown.Item>
+                  </Link>
+                  <Link to="/households">
+                    <Dropdown.Item>Families</Dropdown.Item>
+                  </Link>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Dropdown item text="Meals">
+                <Dropdown.Menu>
+                  <Link to="/meals/new">
+                    <Dropdown.Item>New Meals</Dropdown.Item>
+                  </Link>
+                  <Link to="/meals">
+                    <Dropdown.Item>All Meals</Dropdown.Item>
+                  </Link>
+                </Dropdown.Menu>
+              </Dropdown>
 
-            <Menu.Menu position="right">
-              <Menu.Item
-                name='households'
-                as={Link}
-                to="/households"
-              >
-                Households
-              </Menu.Item>
-
-              <Menu.Item
-                name='meals'
-                as={Link}
-                to="/meals"
-              >
-                Meals
-              </Menu.Item>
-
-              <Menu.Item
-                as={Link}
-                to="/households/new"
-              >
-                New Lead
-              </Menu.Item>
-
-              <Menu.Item
-                as={Link}
-                to="/meals/new">
-                New Meal
-              </Menu.Item>
-
-              <Menu.Item
-                as={Link}
-                to="/"
-                onClick={this.handleLogout}>
+              <Menu.Item as={Link} to="/" onClick={this.handleLogout}>
                 Logout
               </Menu.Item>
-
             </Menu.Menu>
-
-            :
-
-            <Menu.Menu position="right">
-              <Menu.Item
-                as={Link}
-                to="/login">
+          : <Menu.Menu position="right">
+              <Menu.Item as={Link} to="/login">
                 Login
               </Menu.Item>
 
-              <Menu.Item
-                as={Link}
-                to="/signup">
+              <Menu.Item as={Link} to="/signup">
                 Sign Up
               </Menu.Item>
-            </Menu.Menu>
-
-        }
+            </Menu.Menu>}
       </Menu>
     )
   }
@@ -86,10 +57,10 @@ class Nav extends Component {
 
 export default connect(state => {
   return {
-    isAuthenticated: state.auth.isAuthenticated
+    isAuthenticated: state.auth.isAuthenticated,
   }
 }, null)(Nav)
 
 let rules = css({
-  height: '60px'
+  height: '60px',
 })
