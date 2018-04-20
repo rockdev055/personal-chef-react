51fe500019375fcfaf857d2bf31802601586f4fc
rockdev55 <steve264225@gmail.com>
2018-04-19 22:11:02 -0700

    Test css grid in meals container

--- src/containers/MealsContainer.js
+++ src/containers/MealsContainer.js
@@ -4,25 +4,32 @@ import Meal from '../views/Meal'
 import NewMeal from '../components/NewMeal'
 import { Container } from 'semantic-ui-react'
 import { Link, Route, Switch } from 'react-router-dom'
+import { css } from 'glamor'
 
 class MealsContainer extends Component {
   render() {
     return (
-      <Container textAlign="center">
-        <h1>All Meals</h1>
-        <ul>
-          {this.props.meals.map(m =>
-            <Link key={m.id} to={`/meals/${m.id}`}>
-              <h3 style={{ listStyleType: 'none' }}>
-                {m.name}
-              </h3>
-            </Link>
-          )}
-        </ul>
-        <Switch>
-          <Route path="/meals/new" component={NewMeal} />
-          <Route exact path={`/meals/:id`} render={() => <Meal />} />
-        </Switch>
+      <Container>
+        <div {...gridContainer}>
+          <div {...gridNav}>
+            <h1>All Meals</h1>
+            <ul>
+              {this.props.meals.map(m =>
+                <Link key={m.id} to={`/meals/${m.id}`}>
+                  <h3 style={{ listStyleType: 'none' }}>
+                    {m.name}
+                  </h3>
+                </Link>
+              )}
+            </ul>
+          </div>
+          <div {...gridMain}>
+            <Switch>
+              <Route path="/meals/new" component={NewMeal} />
+              <Route exact path={`/meals/:id`} render={() => <Meal />} />
+            </Switch>
+          </div>
+        </div>
       </Container>
     )
   }
@@ -33,3 +40,17 @@ export default connect(state => {
     meals: state.meals,
   }
 })(MealsContainer)
+
+let gridContainer = css({
+  marginTop: '20px',
+  display: 'grid',
+  gridTemplateAreas: `"navigation main main"`,
+})
+
+let gridNav = css({
+  gridArea: 'navigation',
+})
+
+let gridMain = css({
+  gridArea: 'main',
+})
