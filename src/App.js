11d67c7f1eea4f7781a26df77d68283837354d37
rockdev55 <steve264225@gmail.com>
2020-01-19 09:34:48 -0800

    Fix eslint errors in App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { css } from 'glamor';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
@@ -11,15 +12,27 @@ import HouseholdsContainer from './containers/HouseholdsContainer';
 import Background from './images/pexels-photo-349609.jpeg';
 import Signup from './components/Signup';
 import Login from './views/Login';
-import {
-  authenticate,
-  authenticationFailure,
-  logout,
-} from './redux/modules/Auth/actions';
+import { authenticate, authenticationFailure, logout } from './redux/modules/Auth/actions';
 import NewHousehold from './components/NewHousehold';
 import MealsContainer from './containers/MealsContainer';
 import Loading from './components/Loading';
 import { fetchMeals } from './redux/modules/Meals/actions';
+
+const rules = css({
+  width: '100%',
+  height: 'auto',
+  // position: 'fixed',
+  top: 55,
+  left: 0,
+  minHeight: '100%',
+  minWidth: '1024px',
+  background: `url(${Background}) no-repeat center center fixed`,
+  backgroundSize: 'cover',
+  display: 'flex',
+  justifyContent: 'center',
+  color: '#ecf0f1',
+  textShadow: '1px 1px #777',
+});
 
 class App extends Component {
   constructor(props) {
@@ -64,6 +77,14 @@ class App extends Component {
   }
 }
 
+App.propTypes = {
+  fetchMeals: PropTypes.func,
+  authenticate: PropTypes.func,
+  authenticationFailure: PropTypes.func,
+  loading: PropTypes.bool,
+  logout: PropTypes.func,
+};
+
 export default connect(
   state => ({
     loading: state.auth.loading,
@@ -75,19 +96,3 @@ export default connect(
     fetchMeals,
   }
 )(App);
-
-const rules = css({
-  width: '100%',
-  height: 'auto',
-  // position: 'fixed',
-  top: 55,
-  left: 0,
-  minHeight: '100%',
-  minWidth: '1024px',
-  background: `url(${Background}) no-repeat center center fixed`,
-  backgroundSize: 'cover',
-  display: 'flex',
-  justifyContent: 'center',
-  color: '#ecf0f1',
-  textShadow: '1px 1px #777',
-});
