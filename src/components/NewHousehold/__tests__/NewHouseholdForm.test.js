d27434317b9f40d49d9b42a1961c03718a249982
rockdev55 <steve264225@gmail.com>
2020-01-20 13:03:29 -0800

    Test inputs in NewHouseholdForm

--- src/components/NewHousehold/__tests__/NewHouseholdForm.test.js
+++ src/components/NewHousehold/__tests__/NewHouseholdForm.test.js
@@ -9,15 +9,34 @@ afterEach(cleanup);
 const onSubmit = jest.fn();
 
 describe('<NewHouseholdForm />', () => {
-  it('renders correctly', () => {
+  it('submits correctly', () => {
     const { getByTestId, getByText } = render(
       <Provider store={store}>
-        <NewHouseholdForm handleSubmit={onSubmit} />
+        <NewHouseholdForm onSubmit={onSubmit} />
       </Provider>
     );
 
     expect(getByTestId('household-form')).toBeTruthy();
     fireEvent.click(getByText('Create Lead'));
     expect(onSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  it('submits with values', () => {
+    const { getByLabelText, getByText } = render(
+      <Provider store={store}>
+        <NewHouseholdForm onSubmit={onSubmit} />
+      </Provider>
+    );
+    fireEvent.change(getByLabelText('Address'), { target: { value: '123 Main Street' } });
+    fireEvent.change(getByLabelText('Family Name'), { target: { value: 'James' } });
+    fireEvent.change(getByLabelText('Potential Monthly Rate'), { target: { value: '900' } });
+
+    fireEvent.click(getByText('Create Lead'));
+    expect(onSubmit).toHaveBeenCalledWith({
+      address: '123 Main Street',
+      client: false,
+      monthly_rate: '900',
+      name: 'James',
+    });
   });
 });
