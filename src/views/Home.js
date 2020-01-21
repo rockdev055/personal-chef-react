4855e8c0c2baae2683151aaf86e91f672e7544e8
rockdev55 <steve264225@gmail.com>
2020-01-20 23:19:08 -0800

    Remove image from home component

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,23 +3,6 @@ import { css } from 'glamor';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Container, Button } from 'semantic-ui-react';
-import Background from '../images/pexels-photo-349609.jpeg';
-
-const rules = css({
-  width: '100%',
-  height: 'auto',
-  position: 'fixed',
-  top: 55,
-  left: 0,
-  minHeight: '100%',
-  minWidth: '1024px',
-  background: `url(${Background}) no-repeat center center fixed`,
-  backgroundSize: 'cover',
-  display: 'flex',
-  justifyContent: 'center',
-  color: '#ecf0f1',
-  textShadow: '1px 1px #777',
-});
 
 const h1 = css({
   fontSize: '70px',
@@ -35,7 +18,7 @@ const hero = css({
 
 const Home = ({ authenticated, clients }) => (
   <Container textAlign="center">
-    <div {...rules}>
+    <div>
       {authenticated ? (
         <h1>{clients} clients</h1>
       ) : (
